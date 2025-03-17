8f084b9bb3ee520ddc798874b2f5c9d90fd618e8
kolpriks <vladkulikov348@gmail.com>
2025-03-17 21:02:19 +0300

    commit

--- db/docker-entrypoint-initdb.d/init.sql
+++ db/docker-entrypoint-initdb.d/init.sql
@@ -1,13 +1,25 @@
-create table if not exists users (
-	user_id bigserial primary key,
-	first_name varchar(30) not null,
-	second_name varchar(30) not null,
-	email varchar(250) not null unique,
-	password varchar(100) not null
-)
-
 CREATE TABLE cars (
+    id SERIAL PRIMARY KEY, 
+    name VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    image BYTEA,
+    description TEXT,
+    consumption DECIMAL(5, 2) NOT NULL, 
+    seats INT NOT NULL,
+    co2 DECIMAL(5, 2) NOT NULL
+);
+
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    model VARCHAR(255) NOT NULL,
-    price NUMERIC(10, 2) NOT NULL
+    email VARCHAR(255) NOT NULL UNIQUE, 
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE favorite (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    cars_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (cars_id) REFERENCES cars(id) ON DELETE CASCADE
 );
