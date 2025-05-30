203d9bde1773afc830c7ebb3ce1473c050b1a067
kolpriks <vladkulikov348@gmail.com>
2025-05-30 22:38:37 +0300

    выафафывафыа

--- coursework-server/src/main/kotlin/com/coursework_server/coursework_server/controller/CarController.kt
+++ coursework-server/src/main/kotlin/com/coursework_server/coursework_server/controller/CarController.kt
@@ -40,7 +40,7 @@ class CarController(private val carRepository: CarRepository) {
     @DeleteMapping("/{id}")
      fun deleteCar(@PathVariable id: Long): ResponseEntity<Void> {
         return if (carRepository.existsById(id)) {
-            favoriteRepository.deleteByCarsId(id)фыв
+            favoriteRepository.deleteByCarsId(id)
             carRepository.deleteById(id)
             ResponseEntity.noContent().build()
         } else {
